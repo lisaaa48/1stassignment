dc3866f8c6381b85600445aa742c93425c3bae0d
lisahorimoto <horimotorisa@MacBook-Pro.local>
2018-08-13 23:59:28 +0900

    remain two problems

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  	@book = Book.new
+  	@books = Book.all.order(created_at: :asc)
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  	  flash[:success] = 'Book was successfully updated.'
+  	  redirect_to book_path(book)
+  	else
+  	  render("books/edit")
+  	end
+  end
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.valid?
+  	  @book.save
+  	  flash[:success] = 'Book was successfully created.'
+  	  redirect_to book_path(@book)
+	else
+  	  render "index"
+  	end
+  end
+
+  private
+  	def book_params
+  		params.require(:book).permit(:title, :body)
+  	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/books' => 'books#index', as: 'books'
+  post '/books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id/' => 'books#update'
+  delete '/books/:id' => 'books#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
